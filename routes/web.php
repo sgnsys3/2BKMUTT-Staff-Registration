3300c6dbac0ebf0b85a334bad8dcf0a8fd8cb2c8
Pureewat Kaewpoy <pureewat.k@hotmail.com>
2017-01-30 00:29:08 +0700

    closed

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,27 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
-Route::get('/regis/step1', 'StaffRegisterController@step1');
-Route::post('/regis/step2', 'StaffRegisterController@postStep2');
-Route::get('/regis/step2', 'StaffRegisterController@step2');
-Route::get('/regis/step4', 'Step4RegisterController@step4');
-Route::post('/regis/step4', 'Step4RegisterController@uploadDoc');
-Route::get('/regis/quiz', 'Step4RegisterController@quiz');
-Route::get('/logout', 'Step4RegisterController@logout');
-Route::get('/chkregis','StaffRegisterController@checkCompleteUser');
+// Route::get('/', 'HomeController@index');
+// Route::get('/regis/step1', 'StaffRegisterController@step1');
+// Route::post('/regis/step2', 'StaffRegisterController@postStep2');
+// Route::get('/regis/step2', 'StaffRegisterController@step2');
+// Route::get('/regis/step4', 'Step4RegisterController@step4');
+// Route::post('/regis/step4', 'Step4RegisterController@uploadDoc');
+// Route::get('/regis/quiz', 'Step4RegisterController@quiz');
+// Route::get('/logout', 'Step4RegisterController@logout');
+// Route::get('/chkregis','StaffRegisterController@checkCompleteUser');
 
-Route::post('/api/schoolinfo', 'ApiController@schoolInfo');
-Route::post('/api/homeinfo', 'ApiController@homeInfo');
-Route::post('/api/parentinfo', 'ApiController@parentInfo');
-Route::post('/api/2binfo', 'ApiController@a2bInfo');
-Route::post('/api/iscomplete', 'ApiController@isComplete');
-Route::post('/api/answer', 'ApiController@answer');
-Route::post('/api/answercheck', 'ApiController@answerCheck');
+// Route::post('/api/schoolinfo', 'ApiController@schoolInfo');
+// Route::post('/api/homeinfo', 'ApiController@homeInfo');
+// Route::post('/api/parentinfo', 'ApiController@parentInfo');
+// Route::post('/api/2binfo', 'ApiController@a2bInfo');
+// Route::post('/api/iscomplete', 'ApiController@isComplete');
+// Route::post('/api/answer', 'ApiController@answer');
+// Route::post('/api/answercheck', 'ApiController@answerCheck');
 
-Route::post('/api/test', 'ApiController@testApi');
+// Route::post('/api/test', 'ApiController@testApi');
+
+Route::get('', function () {
+    return view('welcome');
+});
 
